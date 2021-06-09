ea9dc6706056a038c139c364ba40e0f1fdfaa838
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 21:39:21 -0300

    Update fakenews.py

--- programas/Lauro/App_Veritas/fakenews.py
+++ programas/Lauro/App_Veritas/fakenews.py
@@ -6,9 +6,9 @@ def veritas():
 #    use_column_width=False, clamp=False, width=1000
     st.header("**Detectando Fake News de COVID-19**")
     st.markdown("---")
-    st.subheader('Cole o texto aqui:')
-    txt = st.text_area("🚀")
-    if st.button("Analisar!"):
-        print('analise carregando o texto')
+    #st.subheader('Cole o texto aqui:')
+    txt = st.text_area("Insira o texto aqui")
+    st.button("Analisar!"):
+        #print('')
     st.markdown("---")
     return predict.predict(txt)
