61b1d6f7b80892894c5ca8a9ca63d054e5b773f0
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 21:00:49 -0300

    Update apresentacao.py

--- programas/Lauro/App_Veritas/apresentacao.py
+++ programas/Lauro/App_Veritas/apresentacao.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 def veritas():
 
-    st.image("https://github.com/m-oxu/dscbc_2021_01-fakenews/blob/main/programas/Lauro/App_Veritas/data/detector.jpg", use_column_width=True, width=[int])
-    st.image("https://github.com/m-oxu/dscbc_2021_01-fakenews/blob/main/programas/Lauro/App_Veritas/data/nossogrupo.JPG", use_column_width=True, width=[int])
-    st.image("https://github.com/m-oxu/dscbc_2021_01-fakenews/blob/main/programas/Lauro/App_Veritas/data/desenho.jpg", use_column_width=True, width=[int])
+    st.image(url("https://github.com/m-oxu/dscbc_2021_01-fakenews/blob/main/programas/Lauro/App_Veritas/data/detector.jpg"), use_column_width=True, width=[int])
+    st.image(url("https://github.com/m-oxu/dscbc_2021_01-fakenews/blob/main/programas/Lauro/App_Veritas/data/nossogrupo.JPG"), use_column_width=True, width=[int])
+    st.image(url("https://github.com/m-oxu/dscbc_2021_01-fakenews/blob/main/programas/Lauro/App_Veritas/data/desenho.jpg"), use_column_width=True, width=[int])
