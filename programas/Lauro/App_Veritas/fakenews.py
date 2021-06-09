dce7870617f25a8be26ed4d921027f3ee3c80604
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 21:08:00 -0300

    Update fakenews.py

--- programas/Lauro/App_Veritas/fakenews.py
+++ programas/Lauro/App_Veritas/fakenews.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def veritas():
-    st.image('https://github.com/m-oxu/dscbc_2021_01-fakenews/blob/main/programas/Lauro/App_Veritas/data/imagem.png', use_column_width=True, width=[int])
+    st.image('App_Veritas/data/imagem.png', use_column_width=True, width=[int])
 #    use_column_width=False, clamp=False, width=1000
     st.header("**Detectando Fake News de COVID-19**")
     st.markdown("---")
