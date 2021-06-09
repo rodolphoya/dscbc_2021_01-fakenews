9c22a189e8e2a134be0f8d04385192f37dec3dd1
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 23:30:35 -0300

    Update predict.py

--- programas/Lauro/App_Veritas/predict.py
+++ programas/Lauro/App_Veritas/predict.py
@@ -8,12 +8,13 @@ import pandas as pd
 import numpy as np
 from functions import ProjectFunctions
 import sklearn
+import streamlit as st
 
 class Predict:
     def predict(txt):
         model = load(open('/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/model.joblib', 'rb'))
         if len(txt) < 70:
-            return('Essa notícia é muito curta! Nosso algoritmo funciona melhor com notícias com 70 caracteres ou mais...')
+            return st.error('Essa notícia é muito curta! Nosso algoritmo funciona melhor com notícias com 70 caracteres ou mais...')
         else:
             texto = (pd.Series(txt)).apply(ProjectFunctions.remover_acentos_e_numeros)
             predicao = ''
@@ -21,4 +22,11 @@ class Predict:
                 predicao = 'verdadeira'
             else:
                 predicao = 'falsa'
-            return (f'A notícia parece ser {predicao}, com {(round(np.max(model.predict_proba(texto)), 2))*100}% de probabilidade.')
+            if (model.predict_proba(texto))*100 > 85:
+                return st.success(f'A notícia parece ser {predicao}, com {(round(np.max(model.predict_proba(texto)), 2))*100}% de probabilidade.')
+            elif (model.predict_proba(texto))*100 < 85 < 70:
+                return st.information(f'A notícia parece ser {predicao}, com {(round(np.max(model.predict_proba(texto)), 2))*100}% de probabilidade.', 
+                                      'Não se esqueça de checar com agências certificadas para ter certeza!')
+            else:
+                  return st.warning(f'A notícia parece ser {predicao}, com {(round(np.max(model.predict_proba(texto)), 2))*100}% de probabilidade.', 
+                                      'É melhor procurar uma agência credenciada para checar essa informação para você, ok?')
