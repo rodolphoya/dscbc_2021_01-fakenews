6c162d5565e2becb145c9341f5a09fb666883764
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 23:17:09 -0300

    Update predict.py

--- programas/Lauro/App_Veritas/predict.py
+++ programas/Lauro/App_Veritas/predict.py
@@ -13,7 +13,7 @@ class Predict:
     def predict(txt):
         model = load(open('/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/model.joblib', 'rb'))
         if len(txt) < 70:
-            print('Essa notícia é muito curta! Nosso algoritmo funciona melhor com notícias com 70 caracteres ou mais...')
+            return('Essa notícia é muito curta! Nosso algoritmo funciona melhor com notícias com 70 caracteres ou mais...')
         else:
             texto = (pd.Series(txt)).apply(ProjectFunctions.remover_acentos_e_numeros)
             predicao = ''
