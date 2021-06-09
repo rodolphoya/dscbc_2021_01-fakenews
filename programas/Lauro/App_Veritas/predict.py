09268dbf2e89b7628d4b6a9a71444a041ca4d438
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 21:47:53 -0300

    Update predict.py

--- programas/Lauro/App_Veritas/predict.py
+++ programas/Lauro/App_Veritas/predict.py
@@ -13,15 +13,16 @@ import numpy as np
 from functions import ProjectFunctions
 import sklearn
 
-def predict(txt):
-    model = load('/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/model.joblib')
-    if len(txt) < 70:
-        print('Essa notícia é muito curta! Nosso algoritmo funciona melhor com notícias com 70 caracteres ou mais...')
-    else:
-        texto = (pd.Series(txt)).apply(ProjectFunctions.remover_acentos_e_numeros)
-        predicao = ''
-        if model.predict(texto) == 'VERDADEIRO':
-            predicao = 'verdadeira'
+class Predict:
+    def predict(txt):
+        model = load('/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/model.joblib')
+        if len(txt) < 70:
+            print('Essa notícia é muito curta! Nosso algoritmo funciona melhor com notícias com 70 caracteres ou mais...')
         else:
-            predicao = 'falsa'
-        return (f'A notícia parece ser {predicao}, com {(round(np.max(model.predict_proba(texto)), 2))*100}% de probabilidade.')
+            texto = (pd.Series(txt)).apply(ProjectFunctions.remover_acentos_e_numeros)
+            predicao = ''
+            if model.predict(texto) == 'VERDADEIRO':
+                predicao = 'verdadeira'
+            else:
+                predicao = 'falsa'
+            return (f'A notícia parece ser {predicao}, com {(round(np.max(model.predict_proba(texto)), 2))*100}% de probabilidade.')
