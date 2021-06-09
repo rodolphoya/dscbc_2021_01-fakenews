a6b96a62932a8388ed7a7f9e965e6b6b29e2e208
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 21:05:30 -0300

    Update apresentacao.py

--- programas/Lauro/App_Veritas/apresentacao.py
+++ programas/Lauro/App_Veritas/apresentacao.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 def veritas():
 
-    st.image("programas/Lauro/App_Veritas/data/detector.jpg"), use_column_width=True, width=[int])
-    st.image("/programas/Lauro/App_Veritas/data/nossogrupo.JPG"), use_column_width=True, width=[int])
-    st.image("/programas/Lauro/App_Veritas/data/desenho.jpg"), use_column_width=True, width=[int])
+    st.image("programas/Lauro/App_Veritas/data/detector.jpg", use_column_width=True, width=[int])
+    st.image("/programas/Lauro/App_Veritas/data/nossogrupo.JPG", use_column_width=True, width=[int])
+    st.image("/programas/Lauro/App_Veritas/data/desenho.jpg", use_column_width=True, width=[int])
