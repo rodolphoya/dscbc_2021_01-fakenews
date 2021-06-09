da74ecb7a838a792016d82a3b178ea3c91c11635
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 23:45:18 -0300

    Update predict.py

--- programas/Lauro/App_Veritas/predict.py
+++ programas/Lauro/App_Veritas/predict.py
@@ -8,8 +8,6 @@ import pandas as pd
 import numpy as np
 from functions import ProjectFunctions
 import sklearn
-import streamlit as st
-
 model = load(open('/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/model.joblib', 'rb'))
 class Predict:
     
