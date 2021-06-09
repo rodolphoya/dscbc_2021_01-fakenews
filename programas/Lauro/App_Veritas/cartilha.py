ee0e76175d034a039dc69464064e7c250e8afccf
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 21:12:11 -0300

    Update cartilha.py

--- programas/Lauro/App_Veritas/cartilha.py
+++ programas/Lauro/App_Veritas/cartilha.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 
 def veritas():
-    st.image("https://github.com/m-oxu/dscbc_2021_01-fakenews/blob/main/programas/Lauro/App_Veritas/data/cartilha.jpeg", use_column_width=True, width=[int])
+    st.image("/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/data/cartilha.jpeg", use_column_width=True, width=[int])
 
