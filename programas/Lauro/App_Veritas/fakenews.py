1ffda988095553cd2c24548e6202fff4a06078d4
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-08 22:52:40 -0300

    Update fakenews.py

--- programas/Lauro/App_Veritas/fakenews.py
+++ programas/Lauro/App_Veritas/fakenews.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from predict import Predict
-def veritas():
-    st.image("/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/data/imagem.png", use_column_width=True, width=[int])
+#def veritas():
+st.image("/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/data/imagem.png", use_column_width=True, width=[int])
 #    use_column_width=False, clamp=False, width=1000
-    st.header("**Detectando Fake News de COVID-19**")
-    st.markdown("---")
-    st.subheader('Cole o texto aqui:')
-    noticia = st.text_input('')
-    if st.button("Analisar!"):
-        pred = Predict.predict(noticia)
-        st.success(pred)
+st.header("**Detectando Fake News de COVID-19**")
+st.markdown("---")
+st.subheader('Cole o texto aqui:')
+noticia = st.text_input('')
+if st.button("Analisar!"):
+    pred = Predict.predict(noticia)
+    st.success(pred)
     #st.markdown("---")
     #return
